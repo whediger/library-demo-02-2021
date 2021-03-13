3df64f1ddbb70816e0c4c1f54e820b61323a0660
Wes Hediger <wes.hediger@galvanize.com>
2021-03-13 10:55:13 -0500

    initial commit

---
+++ src/main/java/com/example/library3/Library3Application.java
@@ -0,0 +1,13 @@
+package com.example.library3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Library3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Library3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/library3/Library3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.library3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Library3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
