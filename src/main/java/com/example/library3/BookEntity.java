6117a7966d947b2b853ad0083177255a6714fcdb
David Grieser <david.grieser@galvanize.com>
2021-03-13 14:54:50 -0600

    swap book storage to JPA Repository

--- src/main/java/com/example/library3/BookEntity.java
+++ src/main/java/com/example/library3/BookEntity.java
@@ -2,6 +2,7 @@ package com.example.library3;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Entity;
@@ -11,6 +12,7 @@ import javax.persistence.Id;
 
 @Entity
 @EqualsAndHashCode
+@NoArgsConstructor
 @Getter
 @Setter
 public class BookEntity {
--- src/main/java/com/example/library3/LibraryController.java
+++ src/main/java/com/example/library3/LibraryController.java
@@ -4,24 +4,25 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class LibraryController {
 
-    ArrayList<BookDto> books;
+    BookService bookService;
 
-    public LibraryController(){
-        books = new ArrayList<BookDto>();
+    public LibraryController(BookService bookService){
+        this.bookService = bookService;
     }
 
     @PostMapping("books")
     @ResponseStatus(HttpStatus.CREATED)
     public void addBooks(@RequestBody BookDto bookDto){
-        books.add(bookDto);
+        this.bookService.create(bookDto);
     }
 
     @GetMapping("books")
-    public ArrayList<BookDto> getBooks(){
-        return this.books;
+    public List<BookDto> getBooks(){
+        return this.bookService.fetchAll();
     }
 }
