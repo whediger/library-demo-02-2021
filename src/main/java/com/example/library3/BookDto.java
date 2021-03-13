c4f7903cb05f5fbebb86d0708b2309f4f2458c8f
Wes Hediger <wes.hediger@galvanize.com>
2021-03-13 11:46:49 -0500

    add persistance

--- src/main/java/com/example/library3/BookDto.java
+++ src/main/java/com/example/library3/BookDto.java
@@ -1,8 +1,12 @@
 package com.example.library3;
 
-import lombok.Value;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Value
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookDto {
     String title;
     String author;
--- src/main/java/com/example/library3/LibraryController.java
+++ src/main/java/com/example/library3/LibraryController.java
@@ -1,19 +1,28 @@
 package com.example.library3;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
 
 @RestController
 public class LibraryController {
 
+    ArrayList<BookDto> books;
+
+    public LibraryController(){
+        books = new ArrayList<BookDto>();
+    }
+
     @PostMapping("books")
     @ResponseStatus(HttpStatus.CREATED)
-    public void addBooks(){
+    public void addBooks(@RequestBody BookDto bookDto){
+        books.add(bookDto);
     }
 
     @GetMapping("books")
-    public String getBooks(){return "[{'title': 'zero to one', 'author':'Blake Masters'}]"; }
+    public ArrayList<BookDto> getBooks(){
+        return this.books;
+    }
 }
