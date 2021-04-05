c4683c0a494b13069fe102d8da295958af28c811
David Grieser <david.grieser@galvanize.com>
2021-04-05 13:31:10 -0500

    asciidoctor setup with static docs

--- src/test/java/com/example/library3/LibraryTestIT.java
+++ src/test/java/com/example/library3/LibraryTestIT.java
@@ -20,6 +20,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs
 public class LibraryTestIT {
 
     @Autowired
